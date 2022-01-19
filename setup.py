3a8a221e2c558b6db4f044077d5925be3306a8b4
Samuel Watkins <35979561+slwatkins@users.noreply.github.com>
2022-01-19 12:02:29 -0800

    add long_description ling to README

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ from setuptools import find_packages, Command
 from numpy.distutils.core import Extension, setup
 
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 upper_files = [
     'upper.pyf',
     'Upper.f',
@@ -70,6 +76,8 @@ setup(
     name="darklim", 
     version="0.1.0", 
     description="DM Limit Setting and Sensitivity", 
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Samuel Watkins", 
     author_email="samwatkins@berkeley.edu", 
     url="https://github.com/slwatkins/darklim", 
