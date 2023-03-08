bb574b6c213a05d458ecdb88741a5c55e6f5640e
Samuel Watkins <35979561+slwatkins@users.noreply.github.com>
2023-03-08 16:07:08 -0700

    update version number to v0.1.2

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ class CleanCommand(Command):
 
 setup(
     name="darklim",
-    version="0.1.1",
+    version="0.1.2",
     description="DM Limit Setting and Sensitivity",
     long_description=long_description,
     long_description_content_type='text/markdown',
