2287d34da1d265a0ae6c5a0b16df21198b2e4c6f
Vetri Velan <vetri@Vetris-MacBook-Pro.local>
2024-06-19 15:04:00 -0700

    Added sapphire band gap as a constant

--- darklim/constants/_constants.py
+++ darklim/constants/_constants.py
@@ -6,3 +6,5 @@ v0_sun = 220e3 # sun velocity about galactic center [m/s]
 ve_orbital = 232e3 # mean orbital velocity of Earth [m/s]
 vesc_galactic = 544e3 # galactic escape velocity [m/s]
 rho0_dm = 0.3 # local DM density [GeV/cm^3]
+
+bandgap_Al203 = 8.8e-3 # Sapphire bandgap [keV]
