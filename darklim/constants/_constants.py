294fdd00d05dfa7238cb262c166e77a97aa59f31
Michael Williams <michaelwilliams@lbl.gov>
2024-09-05 19:52:02 -0700

    fixed for work on tessy

--- darklim/constants/_constants.py
+++ darklim/constants/_constants.py
@@ -6,3 +6,11 @@ v0_sun = 220e3 # sun velocity about galactic center [m/s]
 ve_orbital = 232e3 # mean orbital velocity of Earth [m/s]
 vesc_galactic = 544e3 # galactic escape velocity [m/s]
 rho0_dm = 0.3 # local DM density [GeV/cm^3]
+
+bandgap_GaAs_eV = 1.42
+bandgap_Si_eV = 1.12
+bandgap_Al2O3_eV = 8.8
+Al2O3_density = 3.98
+GaAs_density = 5.32
+GaAs_light_fraction = 0.60
+GaAs_average_phonon_energy_eV = 0.022
